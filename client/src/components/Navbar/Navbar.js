263cd1e1755fbc20b1c09e5120036ee043726a3d
AshishMhrzn10 <amaharjan1032@gmail.com>
2022-04-19 17:24:26 +0545

    Like button updated :rocket:

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
 import memories from '../../images/memories.png';
 
@@ -21,6 +22,13 @@ const Navbar = () => {
 
     useEffect(() => {
         const token = user?.token;
+
+        if (token) {
+            const decodedToken = decode(token);
+            if (decodedToken.exp * 1000 < new Date().getTime()) {
+                logout();
+            }
+        }
 
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
