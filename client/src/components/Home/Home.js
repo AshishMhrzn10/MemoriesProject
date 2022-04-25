0523303d013edb3ab00a8a91108005cd717fb45b
AshishMhrzn10 <amaharjan1032@gmail.com>
2022-04-25 20:28:12 +0545

    update

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -25,9 +25,6 @@ const Home = () => {
     const [search, setSearch] = useState('');
     const [tags, setTags] = useState([]);
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
 
     const searchPost = () => {
         if (search.trim() || tags) {
@@ -79,7 +76,7 @@ const Home = () => {
                         </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                         <Paper elevation={6}>
-                            <Paginate />
+                            <Paginate page={page} />
                         </Paper>
                     </Grid>
                 </Grid>
